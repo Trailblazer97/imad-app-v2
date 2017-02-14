e81be3ff3285db3c666424ba9170e69b8e208638
Trailblazer97 <yashbarapatre97@gmail.com>
2017-02-14 22:28:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the textof the main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+//shistimage to rightbyadding left margin
+var img=document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+};
