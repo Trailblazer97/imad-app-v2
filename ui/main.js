f37f78d227ce768d2753969ce66eb9ccda9b108e
Trailblazer97 <yashbarapatre97@gmail.com>
2017-02-15 23:43:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick=function(){
           //take some action
           if(request.response===200){
               var counter=request.responseText;
-              var span=document.getElementById('count');
+              var span=document.getElementById("count");
               span.innerHTML=counter.toString();
           }
       }  
