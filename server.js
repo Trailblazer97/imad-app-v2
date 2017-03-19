00301e14cbc161e465d03ce3d9da02b09f6fa346
Trailblazer97 <yashbarapatre97@gmail.com>
2017-03-19 21:15:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
+
+var config={
+    user:'trailblazer97',
+    database:'trailblazer97',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -83,8 +92,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool=new Pool(config);
+
 app.get('/test-db', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ //make a select request
+ //return a response with a result
+pool.query('SELECT * FROM test', function(err, result){
+    if(err){
+        send.status(500).send(err.toString());
+    }else{
+        send(JSON.stringify(result));
+    }
+    
+});
+    
 });
 
 var names=[];
